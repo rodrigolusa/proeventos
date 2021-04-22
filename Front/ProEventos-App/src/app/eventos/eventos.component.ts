e312e4e4701303001fd10a369a81ec92192f485c
Rodrigo Lusa <rodrigolusa.17@gmail.com>
2021-04-22 18:24:22 -0300

    Interpolaçao, Diretivas e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-public eventos: any;
+public eventos: any = [];
+public eventosFiltrados: any = [];
+
+  widthImg: number = 100;
+  marginImg: number = 2;
+  showImage: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList() : string{
+    return this._filterList;
+  }
+
+  public set filterList(value:string){
+    this._filterList=value;
+    this.eventosFiltrados = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+  }
+
+  filterEvents(filterBy:string): any{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +39,16 @@ public eventos: any;
     this.getEventos();
   }
 
+  showHideImage(){
+    this.showImage = !this.showImage;
+  }
+
   public getEventos():void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error)
     );
   }
